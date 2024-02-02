2ede594904ded46573df2cfea972886d82e1b7b3
Kelison Rosendo da Rocha <kelison.rocha@zitrus.com.br>
2024-02-01 23:31:46 -0300

    refactor: variável de token passada para o arquivo .env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://api.github.com",
   headers: {
-    Authorization: "token " + "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",
+    Authorization: `token ${import.meta.env.VITE_API_TOKEN}`,
   },
 });
